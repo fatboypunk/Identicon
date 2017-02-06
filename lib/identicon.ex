ca727bf7c256c49004ff998dbe040afdb60aa2a0
Marcel Horlings <marcel.horlings@gmail.com>
2017-02-06 22:36:57 +0100

    create a pixel map which defines where the pixel starts and ends

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -9,6 +9,20 @@ defmodule Identicon do
     |> pick_color
     |> build_grid
     |> filter_odd_squares
+    |> build_pixel_map
+  end
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    pixel_map = Enum.map grid, fn({_code, index}) ->
+      horizontal = rem(index, 5) * 50
+      vertical = div(index, 5) * 50
+
+      top_left = {horizontal, vertical}
+      bottom_right = {horizontal + 50, vertical + 50}
+      {top_left, bottom_right}
+    end
+
+    %Identicon.Image{image | pixel_map: pixel_map}
   end
 
   def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
@@ -16,7 +30,7 @@ defmodule Identicon do
       rem(code, 2) == 0
     end
 
-  %Identicon.Image{image | grid: grid}
+    %Identicon.Image{image | grid: grid}
   end
 
   def build_grid(%Identicon.Image{hex: hex} = image) do
